3179fa6a00cfe6dbc28c62cd9d0f949b4d58d0a6
SangBumShon <103084174+SangBumShon@users.noreply.github.com>
2026-03-27 13:52:41 +0900

    finale of day2 study

--- src/main/java/com/example/practice1/entity/PostEntity.java
+++ src/main/java/com/example/practice1/entity/PostEntity.java
@@ -1,14 +1,19 @@
 package com.example.practice1.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class PostEntity {
 
-    public Long id;
-    public String title;
-    public String content;
-    public String author;
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
 
 
 }
