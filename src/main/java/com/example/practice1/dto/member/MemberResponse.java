0ba7223fc2a34320a0d00643ed1ac6a6e990443b
SangBumShon <103084174+SangBumShon@users.noreply.github.com>
2026-05-02 08:59:37 +0900

    fix: MemberRepository

--- src/main/java/com/example/practice1/dto/member/MemberResponse.java
+++ src/main/java/com/example/practice1/dto/member/MemberResponse.java
@@ -1,4 +1,10 @@
 package com.example.practice1.dto.member;
 
+import com.example.practice1.entity.MemberEntity;
+
 public record MemberResponse(Long id, String loginId, String name) {
+    public static MemberResponse from(MemberEntity member){
+        return new MemberResponse(member.getId(), member.getLoginId(), member.getName());
+    }
+
 }
